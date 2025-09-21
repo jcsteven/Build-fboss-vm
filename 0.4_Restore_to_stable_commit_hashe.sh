4c58ea5e03adc460471f120e6aabdca34ad978ae
jcsteven <jcsteven.yu@gmail.com>
2025-09-21 23:42:59 +0800

    Add 0.4_Restore_to_stable_commit_hashe.sh

---
+++ 0.4_Restore_to_stable_commit_hashe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -------------------------------------------------------
+# Automatic preparation script
+# JC Yu,     June 09,2024
+# -------------------------------------------------------
+# IMPORTANT:
+#   When use: './<this script file>  '
+# -------------------------------------------------------
+source 0.0_comm.sh
+
+TO_STABLE_RELEASE="y"
+if [[ "${TO_STABLE_RELEASE}" == "y" ]]; then
+pushd $GITHUB_WORKSPACE
+    # Remove the stable commit hash pinning if it was done earlier
+    rm -rf build/deps/github_hashes/facebook
+    rm -rf build/deps/github_hashes/facebookincubator
+
+    git stash # This is to save the build-helper changes
+    git pull
+    git stash pop # This is to restore the build-helper changes
+
+    # Optionally ping to latest stable commit hashes
+    tar -xvf fboss/oss/stable_commits/latest_stable_hashes.tar.gz
+popd
+fi
