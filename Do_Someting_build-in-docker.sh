6ccd304aaa9e55840e27c1faa9fdcced638d3b3b
jcsteven <jcyu@accton.com>
2025-09-29 13:39:06 +0800

    Rename shell files which works for Docker container

--- Do_Someting_build-in-docker.sh
+++ Do_Someting_build-in-docker.sh
@@ -9,13 +9,10 @@
 ./0.8_SyncDate.sh
 date
 sleep 2
-./D1_Build_Platform-Service.sh
-./D2_Build_qsfp-service.sh
-./D3_Build_Platform-manager.sh
-./D4_Build_Agent.sh
-./D5_Build_fboss-platform-mapping-gen.sh
-./D6_Build_other-Service.sh
-./D7_Build_fboss.sh
-
-
-
+./D1.1_Build_Platform-Service.sh
+./D1.2_Build_qsfp-service.sh
+./D1.3_Build_Platform-manager.sh
+./D1.4_Build_Agent.sh
+./D1.5_Build_fboss-platform-mapping-gen.sh
+./D1.6_Build_other-Service.sh
+./D1.7_Build_fboss.sh
