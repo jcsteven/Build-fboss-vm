d0268655a3b77019e5c457817e4db0ed0c1e7692
jcsteven <jcyu@accton.com>
2025-09-29 13:43:43 +0800

    Fix D1.8_Build_fboss-all.sh

--- D1.8_Build_fboss-all.sh
+++ D1.8_Build_fboss-all.sh
@@ -7,34 +7,30 @@
 #   When use: './<this script file>  '
 # -------------------------------------------------------
 source 0.0_comm.sh
-
-export GITHUB_ACTIONS_BUILD=1 
-printenv GITHUB_ACTIONS_BUILD
+current_dir=$(pwd)
+TARGET="fboss-all"
+LOG_DIR=${current_dir}/build_log/
+BUILD_LOG=$LOG_DIR/fboss_build-${TARGET}-$(date +"%Y%m%d_%H%M%S").log
+mkdir -p ${LOG_DIR}
 
 cpu_count=$(grep -c ^processor /proc/cpuinfo)
 #cpu_count=16
 echo "CPU Count=$cpu_count"
 
 pushd  $DOCKER_WORKSPACE
-TO_CMD_0="n"
+TO_CMD_0="y"
 if [[ "${TO_CMD_0}" == "y" ]]; then
     cmdl="./build/fbcode_builder/getdeps.py build  --num-jobs $cpu_count"
     cmdl+=' --extra-cmake-defines={"CMAKE_BUILD_TYPE":"MinSizeRel","CMAKE_CXX_STANDARD":"20"}'
-    cmdl+=" --scratch-path ${DOCKER_BUILD_OUTPUT}  --src-dir ./" 
+    cmdl+=" --scratch-path ${DOCKER_BUILD_OUTPUT}"
     cmdl+=" --allow-system-packages" 
-    cmdl+=" --cmake-target  github_actions_fboss_platform_services" 
+    cmdl+=" --src-dir ./"
     cmdl+=" fboss"
-
-    echo "==> $cmdl"
-    $cmdl 
+    export GITHUB_ACTIONS_BUILD=1
+    echo "==> export GITHUB_ACTIONS_BUILD=1" | tee -a $BUILD_LOG
+    printenv GITHUB_ACTIONS_BUILD
 fi
 
-TO_CMD_1="y"
-if [[ "${TO_CMD_1}" == "y" ]]; then
-./build/fbcode_builder/getdeps.py build --allow-system-packages --scratch-path ${DOCKER_BUILD_OUTPUT} --src-dir ./ \
-    --extra-cmake-defines='{"CMAKE_BUILD_TYPE": "MinSizeRel", "CMAKE_CXX_STANDARD": "20","CMAKE_C_COMPILER": "/opt/rh/gcc-toolset-12/root/usr/bin/gcc", "CMAKE_CXX_COMPILER": "/opt/rh/gcc-toolset-12/root/usr/bin/g++"}' \
-    --allow-system-packages \
-    --cmake-target  github_actions_fboss_platform_services \
-  fboss
-fi
+echo "==> $cmdl" 2>&1 | tee -a $BUILD_LOG
+$cmdl  2>&1 | tee -a $BUILD_LOG
 popd
